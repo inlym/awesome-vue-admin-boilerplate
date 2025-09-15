9b7a4ea5fdce1e5f3a2e5ba830cc6bc546113f31
inlym <inlym@qq.com>
2025-09-15 22:22:06 +0800

    引入 axios

---
+++ src/core/http.ts
@@ -0,0 +1,37 @@
+import axios from 'axios'
+
+const http = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  timeout: 100000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+http.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  },
+)
+
+http.interceptors.response.use(
+  (response) => {
+    return response.data
+  },
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem('token')
+      window.location.href = '/login'
+    }
+    return Promise.reject(error)
+  },
+)
+
+export default http
